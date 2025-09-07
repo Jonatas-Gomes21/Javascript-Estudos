de44fab6b0f61df062d3fba03eafcae14adc454c
JonatasGomes <jonatasgomeslima21@gmail.com>
2025-09-07 14:49:47 -0300

    feat: adiciona Loops.js

---
+++ 01-Introducao/Loops.js
@@ -0,0 +1,17 @@
+//Loop WHILE    
+
+var x = 0; // contador inicial do loop
+
+// While = Enquanto. Enquanto 0 for menor que 10. O while vai continuar fazendo o loop. após chegar no numero 9, ele irá encerrar o loop.
+while(x < 10){
+    document.write("<br>O valor de x é: " + x);
+
+    x++; // Vai aumentando o valor de X a cada passada do loop
+}
+
+//Loop FOR
+
+// For = Usado quando sabemos quantas vezes queremos repetir
+for(a = 0; a < valor; a++){
+    document.write("<br> O valor de A = " + a);
+}
