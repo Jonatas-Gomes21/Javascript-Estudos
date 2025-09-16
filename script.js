b65534d62045860459574d249b0217b3cebea993
JonatasGomes <jonatasgomeslima21@gmail.com>
2025-09-15 21:03:29 -0300

    feat: Criaçao da função de verificação de condições em ternário

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-var Mensagem = document.getElementById("Mensagem");
+var numero = 10;
 
-function entrou(){
-    var Nome = prompt("Qual o seu nome?")
-    var Sobrenome = prompt("Qual o seu sobrenome?")
-    if(Nome === "" || Nome === null){
-        alert("Bota o nome ai vai")
-        Mensagem.innerHTML = ("Clica para acessar ai va")
-    }else{
-        Mensagem.innerHTML = ("Seja bem vindo " + Nome + " " + Sobrenome + " ")
-
-        let botaoSair = document.createElement("button");
-        botaoSair.innerText = "Sair da conta";
-        botaoSair.onclick = sair;
-        Mensagem.appendChild(botaoSair);
-    }
-   
-}
-
-function sair(){
-    alert("Ja vai tarde");
-    Mensagem.innerText = ("Ja foi Otario")
-}
+numero === 10 ? console.log("NUMERO É IGUAL A 10") : console.log("NUMERO NÃO TEM O VALOR 10");
