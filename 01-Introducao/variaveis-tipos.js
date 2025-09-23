38a5257515fa0fa913d91fbc5a765a5dc1ea2eff
JonatasGomes <jonatasgomeslima21@gmail.com>
2025-09-23 20:29:50 -0300

    refactor: Adicionando novas variaveis a variaveis-tipo

--- 01-Introducao/variaveis-tipos.js
+++ 01-Introducao/variaveis-tipos.js
@@ -14,4 +14,31 @@ console.log(nome); //A saída após a redeclaração do var.
 nome = "Carlos";
 console.log(nome); // Resultado da retribuição da variável que será mostrado no console.log como Carlos.
 
-//
+//Let
+
+//Uma forma mais moderna de declarar variáveis em JavaScript,
+// que não permite redeclaração, mas permite reatribuição e tem escopo de bloco
+
+let nome2 = "Jonatas";
+console.log(nome2); // o resultado desse console.log será o nome Jonatas.
+
+nome2 = "Maria"; // O let pode ser reatribuído para novos valores.
+console.log(nome2); //A saída após a reatribuição do let, que será mostrado no console.log como Maria.
+
+//Porem, o let não pode ser redeclarado, ou seja, não podemos fazer isso:
+let nome2 = "Carlos"; // Isso gerará um erro.(Redeclaração)
+
+//Const
+
+//Outra forma moderna de declarar variáveis em JavaScript,
+// que não permite redeclaração, não permite reatribuição e tem escopo de bloco
+
+const nome3 = "Jonatas";
+console.log(nome3); // o resultado desse console.log será o nome Jonatas.
+
+// Porem, o const não pode ser reatribuído, ou seja, caso seja feito uma reatribuição ou redeclaração, como abaixo, isso gerará um erro.
+
+nome3 = "Maria"; // Isso gerará um erro.(Reatribuição)
+
+const nome3 = "Carlos"; // Isso também gerará um erro.(Redeclaração)
+
